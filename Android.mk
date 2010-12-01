4df8010f124fa47abf2ee35e1bec98c620cf0aa5
Peter Brinkmann <peter.brinkmann@gmail.com>
2010-12-01 00:16:30 -0500

    corrected Android.mk

--- Android.mk
+++ Android.mk
@@ -28,7 +28,7 @@ LOCAL_SRC_FILES := \
 	pure-data/src/m_conf.c pure-data/src/m_glob.c pure-data/src/m_memory.c \
 	pure-data/src/m_obj.c pure-data/src/m_pd.c pure-data/src/m_sched.c \
 	pure-data/src/s_audio.c pure-data/src/s_audio_dummy.c \
-	pure-data/src/s_entry.c pure-data/src/s_file.c pure-data/src/s_inter.c \
+	pure-data/src/s_file.c pure-data/src/s_inter.c \
 	pure-data/src/s_loader.c pure-data/src/s_main.c pure-data/src/s_path.c \
 	pure-data/src/s_print.c pure-data/src/s_utf8.c pure-data/src/x_acoustics.c \
 	pure-data/src/x_arithmetic.c pure-data/src/x_connective.c \
