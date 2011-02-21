33e0dc722438ea83cb0346570af2a4500f5c9f30
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-02-21 12:17:18 -0500

    added prototypes for inmidi_* functions

--- libpd_wrapper/s_libpdmidi.c
+++ libpd_wrapper/s_libpdmidi.c
@@ -5,6 +5,16 @@
 #include "m_pd.h"
 #include "z_libpd.h"
 
+void inmidi_realtimein(int portno, int cmd);
+void inmidi_byte(int portno, int byte);
+void inmidi_sysex(int portno, int byte);
+void inmidi_noteon(int portno, int channel, int pitch, int velo);
+void inmidi_controlchange(int portno, int channel, int ctlnumber, int value);
+void inmidi_programchange(int portno, int channel, int value);
+void inmidi_pitchbend(int portno, int channel, int value);
+void inmidi_aftertouch(int portno, int channel, int value);
+void inmidi_polyaftertouch(int portno, int channel, int pitch, int value);
+
 #define CLAMP(x, low, high) ((x > high) ? high : ((x < low) ? low : x))
 #define CLAMP4BIT(x) CLAMP(x, 0, 0x0f)
 #define CLAMP7BIT(x) CLAMP(x, 0, 0x7f)
