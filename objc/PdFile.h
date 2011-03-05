575272329ec8b9a35928c947c7e54c5db7a2823f
Rich E <reakinator@gmail.com>
2011-03-05 21:05:59 +1100

    added PdFile objc class for managing a pd file reference

---
+++ objc/PdFile.h
@@ -0,0 +1,31 @@
+//
+//  PdFile.h
+//  libpd
+//
+//  Created by Richard Eakin on 21/02/11.
+//
+//  Copyright (c) 2011 Richard Eakin (reakinator@gmail.com)
+//
+//  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//  WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PdFile : NSObject {
+	NSValue *fileReference_; // pointer to the file
+	int dollarZero_;		 // unique $0 argument assigned by pd
+	NSString *baseName_;	 // stored file base name
+	NSString *pathName_;	 // stored file path name
+}
+
+@property (nonatomic, assign, readonly) int dollarZero;
+@property (nonatomic, copy, readonly) NSString *baseName;
+@property (nonatomic, copy, readonly) NSString *pathName;
+
++ (id)openFileNamed:(NSString *)baseName path:(NSString *)pathName;	// convenience class method for opening a file and returning a reference to self
+- (void)openFile:(NSString *)baseName path:(NSString *)pathName;	// open a file with base and path names
+- (void)closeFile;													// close an opened file, also called in dealloc
+
+@end
---
+++ objc/PdFile.m
@@ -0,0 +1,85 @@
+//
+//  PdFile.m
+//  libpd
+//
+//  Created by Richard Eakin on 21/02/11.
+//
+//  Copyright (c) 2011 Richard Eakin (reakinator@gmail.com)
+//
+//  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//  WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+//
+
+#import "PdFile.h"
+#import "z_libpd.h"
+
+@interface PdFile ()
+
+@property (nonatomic, retain) NSValue *fileReference;
+@property (nonatomic, assign) int dollarZero;
+@property (nonatomic, copy) NSString *baseName;
+@property (nonatomic, copy) NSString *pathName;
+
+@end
+
+@implementation PdFile
+
+@synthesize fileReference = fileReference_;
+@synthesize dollarZero = dollarZero_;
+@synthesize baseName = baseName_;
+@synthesize pathName = pathName_;
+
+#pragma mark -
+#pragma mark - Class Open method
+
++ (id)openFileNamed:(NSString *)baseName path:(NSString *)pathName {
+	PdFile *pdFile = [[[self alloc] init] autorelease];
+	if (pdFile) {
+		[pdFile openFile:baseName path:pathName];
+		if (![pdFile fileReference]) {
+			return nil;
+		}
+	}
+	return pdFile;
+}
+
+#pragma mark -
+#pragma mark - Dealloc
+
+- (void)dealloc {
+	[self closeFile]; 
+	self.pathName = nil;
+	self.baseName = nil;
+	self.fileReference = nil;
+	
+	[super dealloc];
+}
+
+#pragma mark -
+#pragma mark - Public Open / Close methods
+
+- (void)openFile:(NSString *)baseName path:(NSString *)pathName {
+	if (!baseName || !pathName) {
+		return;
+	}
+	self.baseName = baseName;
+	self.pathName = pathName;
+
+	const char *base = [baseName cStringUsingEncoding:NSASCIIStringEncoding];
+	const char *path = [pathName cStringUsingEncoding:NSASCIIStringEncoding];
+	t_pd *x = libpd_openfile(base, path);
+	if (x) {
+		self.fileReference = [NSValue valueWithPointer:x]; 
+		self.dollarZero = libpd_getdollarzero(x);
+	}
+}
+
+- (void)closeFile {
+	t_pd *x = [self.fileReference pointerValue];
+	if (x) {
+		libpd_closefile(x);
+		self.fileReference = nil;
+	}
+}
+
+@end
