7cfac2a30707007c464aa75201ff7174f41349e8
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-11-26 20:59:32 -0500

    incorporating Miller's latest change

--- pure-data/src/g_graph.c
+++ pure-data/src/g_graph.c
@@ -95,8 +95,9 @@ void glist_delete(t_glist *x, t_gobj *y)
             }
             else
             {
-                text_eraseborder(&gl->gl_obj, x,
-                    rtext_gettag(glist_findrtext(x, &gl->gl_obj)));
+                if (glist_isvisible(x))
+                    text_eraseborder(&gl->gl_obj, x,
+                        rtext_gettag(glist_findrtext(x, &gl->gl_obj)));
             }
         }
     }
@@ -107,9 +108,7 @@ void glist_delete(t_glist *x, t_gobj *y)
             glist_getcanvas(x)->gl_name)), 2);
     gobj_delete(y, x);
     if (glist_isvisible(canvas))
-    {
         gobj_vis(y, x, 0);
-    }
     if (x->gl_editor && (ob = pd_checkobject(&y->g_pd)))
         rtext_new(x, ob);
     if (x->gl_list == y) x->gl_list = y->g_next;
@@ -986,7 +985,8 @@ static void graph_delete(t_gobj *z, t_glist *glist)
 {
     t_glist *x = (t_glist *)z;
     t_gobj *y;
-    text_widgetbehavior.w_deletefn(z, glist);
+    if (glist_isvisible(x))
+        text_widgetbehavior.w_deletefn(z, glist);
     while (y = x->gl_list)
         glist_delete(x, y);
 }
