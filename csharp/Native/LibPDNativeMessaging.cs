7b7c8eb7c5709437c6d4f451e6ee08bc6ff02a74
Tebjan Halm <tebjan@vvvv.org>
2012-04-18 08:36:59 +0200

    moved extern methods to there corresponding public methods
    fixed comments
    removed some lines not needed

--- csharp/Native/LibPDNativeMessaging.cs
+++ csharp/Native/LibPDNativeMessaging.cs
@@ -22,16 +22,11 @@ using System.Runtime.CompilerServices;
 namespace LibPDBinding
 {
 	
-
 	//the receiver part of libpd
 	public static partial class LibPD
 	{
-		
 
 		#region Message sending
-		
-		//store bindings
-		private static Dictionary<string, IntPtr> Bindings = new Dictionary<string, IntPtr>();
 		
 		/// <summary>
 		/// If set to true each sent message will be written to Debug output
@@ -53,6 +48,9 @@ namespace LibPDBinding
 		private static bool SWriteMessageToDebug;
 		
 		//sending-----------------------------------------------------------
+				
+		[DllImport("libpd.dll", EntryPoint="libpd_bang")]
+		private static extern  int send_bang([In] [MarshalAs(UnmanagedType.LPStr)] string recv) ;
 		
 		/// <summary>
 		/// sends a bang to the object associated with the given symbol
@@ -66,6 +64,10 @@ namespace LibPDBinding
 			return send_bang(recv);
 		}
 
+				
+		[DllImport("libpd.dll", EntryPoint="libpd_float")]
+		private static extern  int send_float([In] [MarshalAs(UnmanagedType.LPStr)] string recv, float x) ;
+		
 		/// <summary>
 		/// sends a float to the object associated with the given symbol
 		/// </summary>
@@ -78,6 +80,10 @@ namespace LibPDBinding
 		{
 			return send_float(recv, x);
 		}
+		
+				
+		[DllImport("libpd.dll", EntryPoint="libpd_symbol")]
+		private static extern  int send_symbol([In] [MarshalAs(UnmanagedType.LPStr)] string recv, [In] [MarshalAs(UnmanagedType.LPStr)] string sym) ;
 		
 		/// <summary>
 		/// sends a symbol to the object associated with the given symbol
@@ -92,159 +98,72 @@ namespace LibPDBinding
 			return send_symbol(recv, sym);
 		}
 		
+		
+		[DllImport("libpd.dll", EntryPoint="libpd_start_message")]
+		private static extern  int start_message(int max_length) ;
+
+		[DllImport("libpd.dll", EntryPoint="libpd_finish_message")]
+		private static extern  int finish_message([In] [MarshalAs(UnmanagedType.LPStr)] string recv, [In] [MarshalAs(UnmanagedType.LPStr)] string msg) ;
+		
 		/// <summary>
 		/// Sends a message to an object in pd
 		/// </summary>
 		/// <param name="receiver"> </param>
 		/// <param name="message"> </param>
-		/// <param name="args">
-		///            list of arguments of type Integer, Float, or String; no more
-		///            than 32 arguments </param>
+		/// <param name="args"> list of arguments of type Integer, Float, or String </param>
 		/// <returns> error code, 0 on success </returns>
 		[MethodImpl(MethodImplOptions.Synchronized)]
 		public static int SendMessage(string receiver, string message, params object[] args)
 		{
+			var s = "";
+			int err = ProcessArgs(args, ref s);
+			var ret = (err == 0) ? finish_message(receiver, message) : err;
+			
 			if(SWriteMessageToDebug)
 			{
-				var s = String.Format("Message: {0} {1}", receiver, message);
-				int err = ProcessArgs(args, ref s);
-				var ret = (err == 0) ? finish_message(receiver, message) : err;
+				s = String.Format("Message: {0} {1}", receiver, message) + s;
 				s += " Start: " + err;
 				s += " End: " + ret;
 				Debug.WriteLine(s);
-				return ret;
 			}
-			else
-			{
-				var s = "";
-				int err = ProcessArgs(args, ref s);
-				return (err == 0) ? finish_message(receiver, message) : err;
-			}
+		
+			return ret;
 		}
+
+
+		[DllImport("libpd.dll", EntryPoint="libpd_finish_list")]
+		private static extern  int finish_list([In] [MarshalAs(UnmanagedType.LPStr)] string recv) ;
 		
 		/// <summary>
 		/// Sends a list to an object in pd
 		/// </summary>
 		/// <param name="receiver"> </param>
-		/// <param name="args">
-		///            list of arguments of type Integer, Float, or String; no more
-		///            than 32 arguments </param>
+		/// <param name="args"> list of arguments of type Integer, Float, or String </param>
 		/// <returns> error code, 0 on success </returns>
 		[MethodImpl(MethodImplOptions.Synchronized)]
 		public static int SendList(string receiver, params object[] args)
 		{
+			string s = "";
+			int err = ProcessArgs(args, ref s);
+			var ret = (err == 0) ? finish_list(receiver) : err;
+			
 			if(SWriteMessageToDebug)
 			{
-				var s = String.Format("List: {0}", receiver);
-				int err = ProcessArgs(args, ref s);
-				var ret = (err == 0) ? finish_list(receiver) : err;
+				s = String.Format("List: {0}", receiver) + s;
 				s += " Start: " + err;
 				s += " End: " + ret;
 				Debug.WriteLine(s);
-				return ret;
 			}
-			else
-			{
-				var s = "";
-				int err = ProcessArgs(args, ref s);
-				return (err == 0) ? finish_list(receiver) : err;
-			}
-		}
-		
-		/// <summary>
-		/// Get an object array from a space seperated message string threadsafe
-		/// </summary>
-		/// <param name="argsString"></param>
-		/// <returns></returns>
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public static object[] ParseArgsString(string argsString)
-		{
-			return ParseArgsStringUnsync(argsString);
-		}
-		
-		/// <summary>
-		/// Get an object array from a space seperated message string
-		/// This method is not synchronized for performance reasons
-		/// </summary>
-		/// <param name="argsString"></param>
-		/// <returns></returns>
-		public static object[] ParseArgsStringUnsync(string argsString)
-		{
-			var args = argsString.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-			var ret = new object[args.Length];
-			
-			var previousCulture = Thread.CurrentThread.CurrentCulture;
-            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-            
-            for(int i=0; i<args.Length; i++)
-			{
-            	var s = args[i];
-				float f;
-				if(float.TryParse(s, out f))
-				{
-					ret[i] = f;
-				}
-				else
-				{
-					ret[i] = s;
-				}
-			}
-
-			Thread.CurrentThread.CurrentCulture = previousCulture;
 			
 			return ret;
 		}
 		
-		#endregion Message sending
 		
-		#region private
-		
-		[DllImport("libpd.dll", EntryPoint="libpd_bind")]
-		private static extern IntPtr bind([In] [MarshalAs(UnmanagedType.LPStr)] string sym) ;
-		
-		/// Return Type: void
-		///p: void*
-		[DllImport("libpd.dll", EntryPoint="libpd_unbind")]
-		private static extern void unbind(IntPtr p) ;
-		
-		[DllImport("libpd.dll", EntryPoint="libpd_bang")]
-		private static extern  int send_bang([In] [MarshalAs(UnmanagedType.LPStr)] string recv) ;
-		
-		[DllImport("libpd.dll", EntryPoint="libpd_float")]
-		private static extern  int send_float([In] [MarshalAs(UnmanagedType.LPStr)] string recv, float x) ;
-		
-		[DllImport("libpd.dll", EntryPoint="libpd_symbol")]
-		private static extern  int send_symbol([In] [MarshalAs(UnmanagedType.LPStr)] string recv, [In] [MarshalAs(UnmanagedType.LPStr)] string sym) ;
-		
-		/// Return Type: int
-		///max_length: int
-		[DllImport("libpd.dll", EntryPoint="libpd_start_message")]
-		private static extern  int start_message(int max_length) ;
-		
-		/// Return Type: void
-		///x: float
 		[DllImport("libpd.dll", EntryPoint="libpd_add_float")]
 		private static extern  void add_float(float x) ;
-
 		
-		/// Return Type: void
-		///sym: char*
 		[DllImport("libpd.dll", EntryPoint="libpd_add_symbol")]
 		private static extern  void add_symbol([In] [MarshalAs(UnmanagedType.LPStr)] string sym) ;
-
-		
-		/// Return Type: int
-		///recv: char*
-		[DllImport("libpd.dll", EntryPoint="libpd_finish_list")]
-		private static extern  int finish_list([In] [MarshalAs(UnmanagedType.LPStr)] string recv) ;
-
-		
-		/// Return Type: int
-		///recv: char*
-		///msg: char*
-		[DllImport("libpd.dll", EntryPoint="libpd_finish_message")]
-		private static extern  int finish_message([In] [MarshalAs(UnmanagedType.LPStr)] string recv, [In] [MarshalAs(UnmanagedType.LPStr)] string msg) ;
-
 		
 		//parse args helper with debug string
 		private static int ProcessArgs(object[] args, ref string debug)
@@ -284,9 +203,7 @@ namespace LibPDBinding
 			return 0;
 		}
 		
-		
-		
-		#endregion private
+		#endregion Message sending
 		
 	}
 }
