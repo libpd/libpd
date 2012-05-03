0d92e62dcfe2ce1d783f6acc5073f0a4ff2e6142
Tebjan Halm <tebjan@vvvv.org>
2012-05-03 15:38:45 +0200

    added license note

--- libpd_wrapper/z_hookset.c
+++ libpd_wrapper/z_hookset.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012 Tebjan Halm (tebjan@vvvv.org)
+ *
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/libpd/libpd/wiki for documentation
+ *
+ */
 
 #include "z_hookset.h"
 
--- libpd_wrapper/z_hookset.h
+++ libpd_wrapper/z_hookset.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2012 Tebjan Halm (tebjan@vvvv.org)
+ *
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/libpd/libpd/wiki for documentation
+ *
+ */
+
 #ifndef __Z_HOOKSET_H__
 #define __Z_HOOKSET_H__
 
