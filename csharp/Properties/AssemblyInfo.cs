3bd76919710d007db221e8a875b5c237eaddfe2c
Thomas Mayer <thomas@residuum.org>
2016-10-24 00:46:27 +0200

    [C#] Update NuGet package to latests version.

--- csharp/Properties/AssemblyInfo.cs
+++ csharp/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.0.*")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.10.0.*")]
+[assembly: AssemblyInformationalVersion("0.10.0")]
