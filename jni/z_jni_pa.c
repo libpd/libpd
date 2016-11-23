b843183bc0866ebb99d758fdfd1acbca63eb0a01
Peter Brinkmann <peter.brinkmann@gmail.com>
2016-11-22 19:54:12 -0500

    Update to PortAudio v19.

--- jni/z_jni_pa.c
+++ jni/z_jni_pa.c
@@ -11,11 +11,13 @@
 
 #define NTICKS 4
 
-static PortAudioStream *pa_stream = NULL;
+static PaStream *pa_stream;
+static int isRunning = 0;
 
-static int pa_callback(void *inputBuffer, void *outputBuffer,
-                       unsigned long framesPerBuffer, PaTimestamp outTime,
-                       void *userData) {
+static int pa_callback(const void *inputBuffer, void *outputBuffer,
+                       unsigned long framesPerBuffer,
+                       const PaStreamCallbackTimeInfo *timeInfo,
+                       PaStreamCallbackFlags statusFlags, void *userData) {
   pthread_mutex_lock(&mutex);
   libpd_process_float(NTICKS, inputBuffer, outputBuffer);
   pthread_mutex_unlock(&mutex);
@@ -56,15 +58,14 @@ Java_org_puredata_core_PdBase_openAudio(JNIEnv *env, jclass cls, jint inChans,
   pthread_mutex_unlock(&mutex);
   if (err) return err;
   PaError pa_err = Pa_Initialize();
-  if (pa_err != paNoError) return pa_err;
-  pa_err =
-      Pa_OpenDefaultStream(&pa_stream, inChans, outChans, paFloat32, sRate,
-                           NTICKS * libpd_blocksize(), 0, pa_callback, NULL);
+  if (pa_err != paNoError) return -1;
+  pa_err = Pa_OpenDefaultStream(&pa_stream, inChans, outChans, paFloat32, sRate,
+                                NTICKS * libpd_blocksize(), pa_callback, NULL);
   if (pa_err == paNoError) {
     return 0;
   } else {
     Pa_Terminate();
-    return pa_err;
+    return -1;
   }
 }
 
@@ -74,21 +75,32 @@ JNIEXPORT void JNICALL Java_org_puredata_core_PdBase_closeAudio(JNIEnv *env,
     Pa_StopStream(pa_stream);
     Pa_CloseStream(pa_stream);
     pa_stream = NULL;
+    isRunning = 0;
     Pa_Terminate();
   }
 }
 
 JNIEXPORT jint JNICALL Java_org_puredata_core_PdBase_startAudio(JNIEnv *env,
                                                                 jclass cls) {
-  return pa_stream ? Pa_StartStream(pa_stream) : -1;
+  if (pa_stream) {
+    isRunning = 1;
+    return Pa_StartStream(pa_stream) != paNoError;
+  } else {
+    return -1;
+  }
 }
 
 JNIEXPORT jint JNICALL Java_org_puredata_core_PdBase_pauseAudio(JNIEnv *env,
                                                                 jclass cls) {
-  return pa_stream ? Pa_StopStream(pa_stream) : -1;
+  if (pa_stream) {
+    isRunning = 0;
+    return Pa_StopStream(pa_stream) != paNoError;
+  } else {
+    return -1;
+  }
 }
 
 JNIEXPORT jboolean JNICALL Java_org_puredata_core_PdBase_isRunning(JNIEnv *env,
                                                                    jclass cls) {
-  return pa_stream && Pa_StreamActive(pa_stream) > 0;
+  return isRunning;
 }
