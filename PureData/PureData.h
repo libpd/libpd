b85174c8d49a60448ed20a330260c28a44b0a1ae
Paul Williamson <squarefrog@gmail.com>
2016-12-14 20:20:16 +0000

    Add public headers to umbrella header

--- PureData/PureData.h
+++ PureData/PureData.h
@@ -15,5 +15,10 @@ FOUNDATION_EXPORT double PureDataVersionNumber;
 FOUNDATION_EXPORT const unsigned char PureDataVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PureData/PublicHeader.h>
-
-
+#import <PureData/PdAudioUnit.h>
+#import <PureData/PdAudioController.h>
+#import <PureData/AudioHelpers.h>
+#import <PureData/PdMidiDispatcher.h>
+#import <PureData/PdDispatcher.h>
+#import <PureData/PdFile.h>
+#import <PureData/PdBase.h>
