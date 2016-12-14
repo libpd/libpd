2c0b92eb184b8e1c7e66e4dd3597cbd7a76253ab
Paul Williamson <squarefrog@gmail.com>
2016-12-14 19:43:53 +0000

    Create PureData dynamic framework
    Make ObjC headers public

---
+++ PureData/PureData.h
@@ -0,0 +1,19 @@
+//
+//  PureData.h
+//  PureData
+//
+//  Created by Paul Williamson on 14/12/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PureData.
+FOUNDATION_EXPORT double PureDataVersionNumber;
+
+//! Project version string for PureData.
+FOUNDATION_EXPORT const unsigned char PureDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureData/PublicHeader.h>
+
+
