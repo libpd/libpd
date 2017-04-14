6ebb26e270b7e78519296447370210e9baccd674
danomatika <danomatika@gmail.com>
2017-04-13 23:23:04 -0600

    added pdtest_freeverb cpp sample, a project with a working external

---
+++ samples/cpp/pdtest_freeverb/src/PdObject.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2017 Dan Wilcox <danomatika@gmail.com>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/libpd/libpd for documentation
+ *
+ * This file was adapted from the ofxPd openFrameworks addon example:
+ * https://github.com/danomatika/ofxPd
+ *
+ */
+#pragma once
+
+#include "PdBase.hpp"
+
+// custom receiver class
+class PdObject : public pd::PdReceiver, public pd::PdMidiReceiver {
+
+	public:
+
+		// pd message receiver callbacks
+		void print(const std::string& message);
+		
+		void receiveBang(const std::string& dest);
+		void receiveFloat(const std::string& dest, float num);
+		void receiveSymbol(const std::string& dest, const std::string& symbol);
+		void receiveList(const std::string& dest, const pd::List& list);
+		void receiveMessage(const std::string& dest, const std::string& msg, const pd::List& list);
+		
+		// pd midi receiver callbacks
+		void receiveNoteOn(const int channel, const int pitch, const int velocity);
+		void receiveControlChange(const int channel, const int controller, const int value);
+		void receiveProgramChange(const int channel, const int value);
+		void receivePitchBend(const int channel, const int value);
+		void receiveAftertouch(const int channel, const int value);
+		void receivePolyAftertouch(const int channel, const int pitch, const int value);
+		
+		void receiveMidiByte(const int port, const int byte);
+};
---
+++ samples/cpp/pdtest_freeverb/src/externals/Externals.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Dan Wilcox <danomatika@gmail.com>
+ *
+ * BSD Simplified License.
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ *
+ * See https://github.com/libpd/libpd for documentation
+ *
+ * This file was adapted from the ofxPd openFrameworks addon example:
+ * https://github.com/danomatika/ofxPd
+ *
+ */
+#pragma once
+
+// C function forward declarations for C++
+// since these are only in the .c files
+extern "C" {
+	void freeverb_tilde_setup();
+}
