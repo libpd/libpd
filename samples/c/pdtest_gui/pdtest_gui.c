4bc7d10fafcc08b72ed43e4e1b04153af41048fd
danomatika <danomatika@gmail.com>
2019-12-09 16:03:50 +0100

    gettimeofday wrapper for win, fixes #246

--- samples/c/pdtest_gui/pdtest_gui.c
+++ samples/c/pdtest_gui/pdtest_gui.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "z_libpd.h"
+#ifdef _WIN32
+#include "wintimeofday.h" // for gettimeofday()
+#endif
 
 #define NINSTANCES 2
 t_pdinstance *pdinstancevec[NINSTANCES];
---
+++ samples/c/pdtest_gui/wintimeofday.h
@@ -0,0 +1,30 @@
+/*
+ * implementation of POSIX gettimeofday() using the Windows API
+ * from https://stackoverflow.com/a/26085827
+*/
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdint.h> // portable: uint64_t MSVC: __int64 
+
+// MSVC defines this in winsock2.h!?
+typedef struct timeval {
+    long tv_sec;
+    long tv_usec;
+} timeval;
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp) {
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime( &system_time );
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+    return 0;
+}
