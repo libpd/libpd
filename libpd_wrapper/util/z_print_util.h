cefd69be724550a6e69dc9db51c0f4f1551a75f3
danomatika <danomatika@gmail.com>
2022-04-20 00:46:44 +0200

    small print util clarification

--- libpd_wrapper/util/z_print_util.h
+++ libpd_wrapper/util/z_print_util.h
@@ -21,13 +21,12 @@ extern "C"
 #endif
 
 /// assign the pointer to your print line handler
-///
 /// concatenates print messages into single lines before returning them to the
 /// print hook:
 ///   ex: line "hello 123\n" is received in 1 part -> "hello 123"
 /// for comparison, the default behavior may receive messages in chunks:
 ///   ex: line "hello 123" could be sent in 3 parts -> "hello", " ", "123\n"
-///
+/// call with NULL pointer to free internal buffer
 /// note: do not call before libpd_init()
 EXTERN void libpd_set_concatenated_printhook(const t_libpd_printhook hook);
 
